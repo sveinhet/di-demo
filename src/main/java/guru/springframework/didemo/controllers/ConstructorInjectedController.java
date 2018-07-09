63dcd2c34c1aa1a1aae82bf8f0fcb785a8f42f00
sveinhet <sveinhet@gmail.com>
2018-07-09 10:20:22 +0200

    First commit

---
+++ src/main/java/guru/springframework/didemo/controllers/ConstructorInjectedController.java
@@ -0,0 +1,15 @@
+package guru.springframework.didemo.controllers;
+
+import guru.springframework.didemo.services.GreetingService;
+
+public class ConstructorInjectedController {
+    private GreetingService greetingService;
+
+    public ConstructorInjectedController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String sayHello() {
+        return greetingService.sayGreeting();
+    }
+}
---
+++ src/test/java/guru/springframework/didemo/controllers/SetterInjectedControllerTest.java
@@ -0,0 +1,22 @@
+package guru.springframework.didemo.controllers;
+
+import guru.springframework.didemo.services.GreetingServiceImpl;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SetterInjectedControllerTest {
+    private SetterInjectedController setterInjectedController;
+
+    @Before
+    public void setUp() {
+        setterInjectedController = new SetterInjectedController();
+        setterInjectedController.setGreetingService(new GreetingServiceImpl());
+    }
+
+    @Test
+    public void testGreeting() {
+        assertEquals(GreetingServiceImpl.HELLO_GURUS, setterInjectedController.greetingService.sayGreeting());
+    }
+}
