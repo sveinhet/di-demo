2a138b7c6fd907bbe4b772b58ebc273eb8825ede
sveinhet <sveinhet@gmail.com>
2018-07-09 10:43:56 +0200

    Lagt til autowiring

--- src/main/java/guru/springframework/didemo/controllers/ConstructorInjectedController.java
+++ src/main/java/guru/springframework/didemo/controllers/ConstructorInjectedController.java
@@ -1,10 +1,14 @@
 package guru.springframework.didemo.controllers;
 
 import guru.springframework.didemo.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class ConstructorInjectedController {
     private GreetingService greetingService;
 
+    @Autowired
     public ConstructorInjectedController(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
--- src/main/java/guru/springframework/didemo/controllers/PropertyInjectedController.java
+++ src/main/java/guru/springframework/didemo/controllers/PropertyInjectedController.java
@@ -1,8 +1,12 @@
 package guru.springframework.didemo.controllers;
 
 import guru.springframework.didemo.services.GreetingServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class PropertyInjectedController {
+    @Autowired
     public GreetingServiceImpl greetingService;
 
     public String sayHello() {
